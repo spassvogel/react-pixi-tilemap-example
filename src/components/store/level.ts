5fb3b9b9d09a88156c1d52723834cf0617db474a
Wouter van den Heuvel <exo_00@hotmail.com>
2025-07-25 19:39:30 +0200

    install devtools middlware

--- src/components/store/level.ts
+++ src/components/store/level.ts
@@ -1,13 +1,17 @@
 import { create } from 'zustand'
+import { devtools } from 'zustand/middleware'
 
 type LevelStore = {
   cameraX: number
   setCameraX: (x: number) => void
 }
 
-export const useLevelStore = create<LevelStore>((set) => ({
-  cameraX: 0,
-
-  setCameraX: (x) => set((state) => ({ ...state, cameraX: x }))
-}))
+export const useLevelStore = create<LevelStore>()(
+  devtools(
+    (set) => ({
+      cameraX: 0,
+      setCameraX: (x) => set((state) => ({ ...state, cameraX: x }))
+    })
+  )
+)
 
